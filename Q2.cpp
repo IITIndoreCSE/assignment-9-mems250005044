8c9d0101630ef59c3072238d174aeaa6a567644c
mems250005044 <mems250005044@iiti.ac.in>
2025-10-31 11:01:47 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Color {
+    int red, green, blue;
+};
+
+int main() {
+    Color colors[5] = {
+        {120, 60, 200},
+        {255, 100, 0},
+        {0, 255, 128},
+        {75, 75, 75},
+        {10, 20, 30}
+    };
+
+    // Invert first 3 colors
+    for (int i = 0; i < 3; i++) {
+        colors[i].red   = 255 - colors[i].red;
+        colors[i].green = 255 - colors[i].green;
+        colors[i].blue  = 255 - colors[i].blue;
+    }
+
+    cout << "Inverted color values:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Color " << i + 1 << " -> "
+             << "R: " << colors[i].red << ", "
+             << "G: " << colors[i].green << ", "
+             << "B: " << colors[i].blue << endl;
+    }
+
+    return 0;
+}
 
